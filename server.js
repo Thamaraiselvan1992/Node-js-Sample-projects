0a32797654935784b488a85d1e102af9eff74bdf
Thamaraiselvan1992 <tsthamarai4@gmail.com>
2019-05-29 15:21:43 +0530

    first comment

---
+++ server.js
@@ -0,0 +1,23 @@
+
+const express=require('express')
+const app=express()
+const expresslayout=require('express-ejs-layouts')
+const mongoose=require('mongoose')
+const indexRouter=require('./routes/index')
+app.set('view engine','ejs')
+app.set('views',__dirname+'/views')
+app.set('layout','layouts/layout')
+app.use(expresslayout)
+app.use(express.static('public'))
+mongoose.connect("mongodb+srv://Thamaraiselvan:"+process.env.MongoDB_URL+"@cluster0-66r7n.mongodb.net/test?retryWrites=true",{
+    useNewUrlParser:true
+})
+const db=mongoose.connection;
+db.on('error',error=>{
+console.error(error)
+});
+db.once('open',()=>{
+    console.log('mongoose connected')
+})
+app.use('/',indexRouter)
+app.listen(process.env.PORT||3000)
